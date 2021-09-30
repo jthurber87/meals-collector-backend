e5ada7ba1863366b5a6f3f3d4d7dc18acbb10de1
Froylan <froy.push@gmail.com>
2021-09-29 19:36:46 -0500

    Full crud on REACT side and Backend

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
+//dep
+require('dotenv').config()
 require('./db/db')
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const mealsController = require('./controllers/mealsCollector.js')
-const PORT = 9000
+const PORT = process.env.PORT || 9000
 
 // middleware
+const whiteList = ["http://localhost:3000"]
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whiteList.includes(origin) || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  }
+}
+app.use(cors(corsOptions))
 app.use(express.json());
 app.use('/meals', mealsController)
 
