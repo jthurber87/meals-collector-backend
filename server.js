56bc6f9e8f96af0e8cf0feab6cc254912ae070a3
Jay Thurber <jthurber87@gmail.com>
2021-09-29 23:44:27 -0600

    fetch req working

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
-require('./db/db')
+const cors = require("cors");
 const express = require('express')
 const app = express()
-const mealsController = require('./controllers/mealsCollector.js')
 const PORT = 9000
+const mealsController = require('./controllers/mealsCollector.js')
+require('./db/db')
+const whitelist = ["http://localhost:3000"];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) != -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+};
 
 // middleware
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/meals', mealsController)
+
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
